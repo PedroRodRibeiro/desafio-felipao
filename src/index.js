43d03e60f5e9e855d6acd2a9363ef7ca19f7986b
pedro-hrr <p.rodovalhoribeiro@gmail.com>
2024-12-17 08:53:51 -0300

    desafio do felipão finalizado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-let nome = "pedro";
+let nomeHeroi = "Pantera Negra";
+let qtdXp = 5000;
+let mostraXP = " ";
 
-console.log(nome);
-console.log("oi pexual");
+if (qtdXp <= 1000){
+    mostraXP = "Ferro";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 1001) && (qtdXp <= 2000)){
+    mostraXP = "Bronze";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 2001) && (qtdXp <= 5000)){
+    mostraXP = "Prata";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 5001) && (qtdXp <= 7000)){
+    mostraXP = "Ouro";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 7001) && (qtdXp <= 8000)){
+    mostraXP = "Platina";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 8001) && (qtdXp <= 9000)){
+    mostraXP = "Ascendente";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else if ((qtdXp >= 9001) && (qtdXp <= 10000)){
+    mostraXP = "Imortal";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+
+} else {
+    mostraXP = "Radiante";
+    console.log("O Herói de nome **" + nomeHeroi + "** está no nível de **" + mostraXP + "**")
+}
+
+
+
+
+
